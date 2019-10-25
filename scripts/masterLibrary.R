1dfae45751a4e6942a83016bf3da5e279281c429
jbeaulie <beaulieu.jake@epa.gov>
2019-10-25 13:47:53 -0400

    Adding 2016 multi res survey data

--- scripts/masterLibrary.R
+++ scripts/masterLibrary.R
@@ -1,6 +1,9 @@
 library(sf) # for spatial data
 library(tidyverse) # dplyr, ggplot
+library(reshape2)
 library(readxl) # read_excel
 library(USAboundaries) # for states map
 library(spsurvey) # for GRTS design
 
+# returns string w/o leading or trailing whitespace
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
