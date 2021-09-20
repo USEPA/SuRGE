3027ef92f43db922b3ec4e3019a192504a2a2fd0
Beaulieu <Beaulieu.Jake@epa.gov>
2021-09-20 12:27:36 -0400

    adding conflicted function management

--- scripts/masterLibrary.R
+++ scripts/masterLibrary.R
@@ -6,3 +6,7 @@ library(plotly) # interactive plots (readLgr.R)
 library(spsurvey) # lake design
 library(leaflet) # for lake design printables
 library(mapview) # for lake design printables
+
+library(conflicted)
+conflict_prefer("select", "dplyr") # select() will call dplyr::select()
+conflict_prefer("filter", "dplyr") # filter() will call dplyr::filter()
