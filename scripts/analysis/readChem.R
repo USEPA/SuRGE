5187ce1ce9872187735ddddbf878c20c98b52876
Corra <Corra.Joseph@epa.gov>
2021-10-25 09:50:51 -0400

    Push current code to repo for Pegasus.

--- scripts/analysis/readChem.R
+++ scripts/analysis/readChem.R
@@ -52,40 +52,55 @@ toc.epa <- rbind(toc.1, toc.2, toc.3) %>% janitor::clean_names()
 # str_length + str_squish revealed no leading or trailing spaces in toc.epa
 
 
-toc.epa.foo <- toc.epa %>% 
-  mutate(
-    # split sample_id into two fields
-    sample_id1 = str_split_fixed(sample_id, pattern = " ", n=2)[,1], # numeric ID first
-    lab_qa = str_split_fixed(sample_id, pattern = " ", n=2)[,2], # character values
-    # 202798d2 is flagged as rerun, but I can't find the original injection.
-    # I think we just ignore this flag.  Here we replace "rerun" with NA.
-    # Also replacing "" with NA to clean up this column
-    lab_qa = replace(lab_qa, 
-                     (sample_id1 == "202798d2" & lab_qa == "rerun") | lab_qa == "", NA)) %>%
+# toc.epa.foo <- toc.epa %>% 
+#   mutate(
+#     # split sample_id into two fields
+#     sample_id1 = str_split_fixed(sample_id, pattern = " ", n=2)[,1], # numeric ID first
+#     lab_qa = str_split_fixed(sample_id, pattern = " ", n=2)[,2], # character values
+#     # 202798d2 is flagged as rerun, but I can't find the original injection.
+#     # I think we just ignore this flag.  Here we replace "rerun" with NA.
+#     # Also replacing "" with NA to clean up this column
+#     lab_qa = replace(lab_qa, 
+#                      (sample_id1 == "202798d2" & lab_qa == "rerun") | lab_qa == "", NA)) %>%
+#   
+#   
+#     #203613 was run on 7/24 and noted "will be re-analyzed this week"
+#     #it was rerun on 7/31/21 with the sampleID 203613rerun.  I believe it was rerun
+#     #because it was below the lowest value in the standard curve.  The two values
+#     #are similar, so lets treat as lab DUPS.
+#   mutate(
+#     notes = replace(notes, sample_id1 == "203613rerun", "rerun because initial results below std curve"),
+#     lab_qa = replace(lab_qa, sample_id1 == "203613rerun", "DUP"),
+#     sample_id1 = replace(sample_id1, sample_id1 == "203613rerun", 203613)) %>%
+#   # remove lab qa.qc not needed
+#   filter(nchar(sample_id1) > 5, # values with less than 5 characters are lab qa.qc
+#          lab_qa != "SPK" | is.na(lab_qa)) # omit lab spikes, but must specify to keep NA
+
+# Integrated Jake/Joe version (18 Oct 2021) working version. 
+toc.epa <- rbind(toc.1, toc.2, toc.3) %>% 
+  janitor::clean_names() %>%
+  # extract lab ID and put it in a new column:
+  mutate(sample_id1 = as.numeric(str_split_fixed(sample_id, pattern = "\\D", n=2)[,1])) %>%
+  filter(is.na(sample_id1) == FALSE) %>%  # filter out anything without a lab id
+  mutate(lab_qa = str_extract(sample_id, pattern = "SPK|DUP|d2")) %>% # id the dupes
+  select(-sample_id) %>%
+  relocate(sample_id1) %>%
+  #203613 was run on 7/24 and noted "will be re-analyzed this week"
+  #it was rerun on 7/31/21 with the sampleID 203613rerun.  I believe it was rerun
+  #because it was below the lowest value in the standard curve.  The two values
+  #are similar, so lets treat as lab DUPS.
+  mutate(notes = replace(notes, sample_id1 == "203613", "rerun because initial results below std curve"))
+  # 18 Oct 2021: don't filter anything else yet
+  # filter(dup_sample == "DUP" | is.na(dup_sample)==TRUE) # filter out the d2 and SPK
+
+# add-in the following to combine the duplicates. 
+  # group_by(unit, lab_id) %>%  # PROBLEM: we lose some columns this way.
+  # summarize(npoc_result = mean(npoc_result))
+
+
+toc_joined <- left_join(toc.epa, chemCoc, by = c("sample_id1" = "lab_id"))
+
   
-  
-    #203613 was run on 7/24 and noted "will be re-analyzed this week"
-    #it was rerun on 7/31/21 with the sampleID 203613rerun.  I believe it was rerun
-    #because it was below the lowest value in the standard curve.  The two values
-    #are similar, so lets treat as lab DUPS.
-  mutate(
-    notes = replace(notes, sample_id1 == "203613rerun", "rerun because initial results below std curve"),
-    lab_qa = replace(lab_qa, sample_id1 == "203613rerun", "DUP"),
-    sample_id1 = replace(sample_id1, sample_id1 == "203613rerun", 203613)) %>%
-  # remove lab qa.qc not needed
-  filter(nchar(sample_id1) > 5, # values with less than 5 characters are lab qa.qc
-         lab_qa != "SPK" | is.na(lab_qa)) # omit lab spikes, but must specify to keep NA
-
-# left_join(toc.epa.foo, chemCoc, by = c("sample_id1" = "lab_id"))
-
-
-z2 <- str_extract(toc.epa$sample_id, pattern = "\\-*\\d+\\.*\\d*") %>%
-  as.numeric() %>%
-  as.data.frame() %>%
-  filter(.>1000)
-  
-
-
 
 # NUTRIENTS-----------------------
 # Nutrient samples for the 2020 SuRGE field season were held in Cincinnati,
