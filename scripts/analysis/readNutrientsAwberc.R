ae5ad85e33d172756f89c2b89d3f60beb929b235
Beaulieu <Beaulieu.Jake@epa.gov>
2021-11-16 15:22:10 -0500

    Resolving conflicts between local and remote

---
+++ scripts/analysis/readNutrientsAwberc.R
@@ -0,0 +1,7 @@
+# NUTRIENT ANALYSIS ON LACHAT CONDUCTED IN AWBERC
+
+# Original data can be found at: L:\Priv\Cin\ORD\Pegasus-ESF\Lachat Data\Nutrients
+# Copy of orignal data file at SP: 
+# ....data\chemistry\nutrients\2021_ESF-EFWS_NutrientData_Updated11082021_AKB.xlsx"
+
+# see readChem.R in mulitResSurvey repo for an example of how to read data.
