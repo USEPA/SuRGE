d910fec78cb451a8023878fa8500d79cbde9476c
Corra <Corra.Joseph@epa.gov>
2021-12-08 16:41:47 -0500

    Created second get_ada_data function to read-in data from 2020 (and presumably 2022 onward). Added code to filter out extra rows (lab notes at bottom of spreadsheet data). Began modifying code to add a qual field for analyte holding times.

--- scripts/analysis/readNutrientsAda.R
+++ scripts/analysis/readNutrientsAda.R
@@ -43,7 +43,9 @@ get_ada_data21 <- function(path, datasheet) {
       mutate(Analytes = str_c(Analytes, Unit)) %>% # concatenate analyte and unit, so unit is retained
       column_to_rownames(var = "Analytes") %>% # simpler to work w/ rownames in next chunk of code
       mutate(MDL = as.numeric(MDL)) # covert MDL values to numeric
-   
+
+analyte_names <- row.names(toptable) # pass analyte names to maintable, below
+
    #'maintable' combines 'toptable' with results
    maintable <- read_excel(paste0(path, datasheet), # get the results
                            sheet = "Data", range = "A14:N500") %>% # up to 492 rows
@@ -51,32 +53,36 @@ get_ada_data21 <- function(path, datasheet) {
       janitor::clean_names() %>%
       mutate(lab_sample_id = toupper(lab_sample_id)) %>% # make uppercase since Ada isn't consistent
       mutate(labdup = if_else(str_detect(lab_sample_id, "LAB DUP"), "LAB DUP", "")) %>% # flag the dups
-      select(field_sample_id, labdup, starts_with("data")) %>% # remove unneeded columns
-      rename_with(~row.names(toptable), .cols = starts_with("data")) %>% # rename using analyte names
+      select(field_sample_id, labdup, starts_with("dat")) %>% # remove unneeded columns
+      rename_with(~paste0(analyte_names), .cols = starts_with("data")) %>% # rename using analyte names
+      rename_with(~paste0(analyte_names, "_date_analyzed"), .cols = starts_with("date_a")) %>% # rename using analyte names
+      mutate(across(ends_with("analyzed"), # compute holding time of analytes
+                    ~ as.numeric(as.Date(., format = "%m/%d/%Y") - as.Date(date_collected, format = "%m/%d/%Y")))) %>%
       rename(sampleid = field_sample_id) %>% # temporary rename; changes later during text parsing
-      select(sampleid, labdup, everything()) %>% # reorder columns for the following mutate() 
-      mutate(across(3:last_col(), # create new flag column if analyte not detected
-                    ~ if_else(str_detect(., "ND"), "<", ""),
-                    .names = "{col}_flag")) %>%
-      mutate(across(3:last_col(), # replace ND with the MDL value from toptable
-                    ~ ifelse(str_detect(., "ND"), toptable[paste(cur_column()),1], .))) %>% # note this is base::ifelse
-      mutate(sampleid = str_replace_all(sampleid, "(TN or DN)","")) %>% # clean-up sampleid field
-      mutate(sampleid = str_replace_all(sampleid, "\\(\\)","")) %>% # clean-up sampleid field
-      mutate(sampleid = str_replace_all(sampleid, " ", "")) %>% # remove any blank spaces inside string
-      mutate(across(!ends_with(c("flag", "labdup", "sampleid")), # remove 'BQL', 'RPD' & other junk from data fields
-                    ~ str_extract(., pattern = "\\-*\\d+\\.*\\d*"))) %>%
-      mutate(across(!ends_with(c("flag", "labdup", "sampleid")), # make extracted data numeric
-                    ~ as.numeric(.))) %>%
-      #janitor::clean_names()  %>% # this is causing a problem by changing mu symbol to 'm'
-      mutate(sample_depth = str_sub(sampleid, 4, 4)) %>% # get sample depth from sampleid
-      mutate(sample_type = str_sub(sampleid, 5, 5)) %>% # get sample type from sampleid
-      mutate(sampleid = str_sub(sampleid, 1, 3)) %>% # make sampleid 3-digit numeric lake id only
-      mutate(sample_depth = str_replace_all(sample_depth, c("D" = "deep", "S" = "shallow", "N" = "blank"))) %>%
-      mutate(sample_type = str_replace_all(sample_type, c("B" = "blank", "U" =  "unknown", "D" =  "duplicate"))) %>%
-      dplyr::rename(lake_id = sampleid) %>% # change name to match chemCoc
-      mutate(site_id = "") %>% # create empty column for site_id (id is populated later)
-      select(order(colnames(.))) %>% # alphabetize column names
-      select(lake_id, site_id, sample_depth, sample_type, labdup, everything()) # put id fields first
+      filter(str_starts(sampleid, "\\(")) %>% # retain only rows where sampleid starts with '('
+      select(sampleid, labdup, everything(), -date_collected) #%>% # reorder columns for the following mutate()
+      # mutate(across(4:last_col(), # create new flag column if analyte not detected
+      #               ~ if_else(str_detect(., "ND"), "<", ""),
+      #               .names = "{col}_flag")) %>%
+      # mutate(across(4:last_col(), # replace ND with the MDL value from toptable
+      #               ~ ifelse(str_detect(., "ND"), toptable[paste(cur_column()),1], .))) %>% # note this is base::ifelse
+      # mutate(sampleid = str_replace_all(sampleid, "(TN or DN)","")) %>% # clean-up sampleid field
+      # mutate(sampleid = str_replace_all(sampleid, "\\(\\)","")) %>% # clean-up sampleid field
+      # mutate(sampleid = str_replace_all(sampleid, " ", "")) %>% # remove any blank spaces inside string
+      # mutate(across(!ends_with(c("flag", labdup", "sampleid")), # remove 'BQL', 'RPD' & other junk from data fields
+      #               ~ str_extract(., pattern = "\\-*\\d+\\.*\\d*"))) %>%
+      # mutate(across(!ends_with(c("flag", "labdup", "sampleid")), # make extracted data numeric
+      #               ~ as.numeric(.))) %>%
+      # #janitor::clean_names()  %>% # this is causing a problem by changing mu symbol to 'm'
+      # mutate(sample_depth = str_sub(sampleid, 4, 4)) %>% # get sample depth from sampleid
+      # mutate(sample_type = str_sub(sampleid, 5, 5)) %>% # get sample type from sampleid
+      # mutate(sampleid = str_sub(sampleid, 1, 3)) %>% # make sampleid 3-digit numeric lake id only
+      # mutate(sample_depth = str_replace_all(sample_depth, c("D" = "deep", "S" = "shallow", "N" = "blank"))) %>%
+      # mutate(sample_type = str_replace_all(sample_type, c("B" = "blank", "U" =  "unknown", "D" =  "duplicate"))) %>%
+      # rename(lake_id = sampleid) %>% # change name to match chemCoc
+      # mutate(site_id = "") %>% # create empty column for site_id (id is populated later)
+      # select(order(colnames(.))) %>% # alphabetize column names
+      # select(lake_id, site_id, sample_depth, sample_type, labdup, everything()) # put id fields first
 
   return(maintable)
 
@@ -87,20 +93,20 @@ get_ada_data21 <- function(path, datasheet) {
 dup_agg <- function(data) {
    
    # carve out the _flag and _units columns so they can be re-joined later
-   c <- data %>% select(ends_with(c("id","labdup", "type", "depth", "flag", "filter", "units")))
+   c <- data %>% select(ends_with(c("id","labdup", "type", "depth", "flag", "filter", "units", "qual")))
    
    # group and summarize to obtain means                
    d <- data %>%
       dplyr::group_by(sample_depth, sample_type) %>%
       # '!' to exclude columns we don't want to aggregate
-      summarize(across(!ends_with(c("id","labdup", "type", "depth", "flag", "filter", "units")), 
+      summarize(across(!ends_with(c("id","labdup", "type", "depth", "flag", "filter", "units", "qual")), 
                        ~ mean(., na.rm = TRUE)))
    
    e <- left_join(d, c, by = c("sample_depth", "sample_type")) %>% # rejoin the data
       mutate(across(3:last_col(), # convert NaN to NA
                     ~ ifelse(is.nan(.), NA, .))) %>% # must use ifelse here (not if_else)
       select(order(colnames(.))) %>% # alphabetize column names
-      select(lake_id, site_id, sample_depth, sample_type, sample_filter, labdup, everything()) %>% # put 'sampleid' first
+      select(lake_id, site_id, sample_depth, sample_type, sample_filter, labdup, qual, everything()) %>% # put 'sampleid' first
       filter(labdup != "LAB DUP") %>% # remove the lab dup; we may want to revisit this.
       select(-labdup) # remove labdup column. JB 12/7/2021
    # note that the LAB DUP and the original now have identical values, but the < flags may differ
@@ -120,7 +126,9 @@ conv_units <- function(data, filename) {
 # filename <- toupper(filename) # use if case becomes an issue in file names;
       # note that 'oP' (in code below) contains a lowercase letter. 
 
-
+# this is probably a good place to change qual to qual_group1, group2, et al.
+   # add the following code to each if:
+   # rename(qual_group1 = qual)     
    
    # AMMONIUM, NITRATE, NITRITE
    if (str_detect(paste(filename), "NH4")) 
@@ -261,7 +269,7 @@ zzz <- left_join(jea1, jea2, by = "lake_id")
 
 get_ada_data <- function(path, datasheet) { 
    
-# for 2020 data and 2022 onward if field_sample_id format is the same
+# for 2020 data and 2022 onward, assuming field_sample_id format is the same
    
    #'toptable' contains analyte names and MDL values
    toptable <- read_excel(paste0(path, datasheet), # get MDL & analyte names
@@ -286,18 +294,22 @@ get_ada_data <- function(path, datasheet) {
       janitor::clean_names() %>%
       mutate(lab_sample_id = toupper(lab_sample_id)) %>% # make uppercase since Ada isn't consistent
       mutate(labdup = if_else(str_detect(lab_sample_id, "LAB DUP"), "LAB DUP", "")) %>% # flag the dups
-      select(field_sample_id, labdup, starts_with("data")) %>% # remove unneeded columns
+      mutate(htime = as.Date(date_analyzed, format = "%m/%d/%Y") - 
+                as.Date(date_collected, format = "%m/%d/%Y")) %>% # compute sample holding time
+      mutate(qual = if_else(htime > 28, "HOLD TIME EXCEEDED!", "GOOD!")) %>% # flag if hold time exceeded
+      select(field_sample_id, labdup, qual, starts_with("data")) %>% # remove unneeded columns
       rename_with(~row.names(toptable), .cols = starts_with("data")) %>% # rename using analyte names
       rename(sampleid = field_sample_id) %>% # temporary rename; changes later during text parsing
+      filter(str_starts(sampleid, "DN|TN")) %>% # retain only rows where sampleid starts with TN or DN
       select(sampleid, labdup, everything()) %>% # reorder columns for the following mutate() 
-      mutate(across(3:last_col(), # create new flag column if analyte not detected
+      mutate(across(4:last_col(), # create new flag column if analyte not detected
                     ~ if_else(str_detect(., "ND"), "<", ""),
                     .names = "{col}_flag")) %>%
-      mutate(across(3:last_col(), # replace ND with the MDL value from toptable
+      mutate(across(4:last_col(), # replace ND with the MDL value from toptable
                     ~ ifelse(str_detect(., "ND"), toptable[paste(cur_column()),1], .))) %>% # note this is base::ifelse
-      mutate(across(!ends_with(c("flag", "labdup", "sampleid")), # remove 'BQL', 'RPD' & other junk from data fields
+      mutate(across(!ends_with(c("flag", "qual", "labdup", "sampleid")), # remove 'BQL', 'RPD' & other junk from data fields
                     ~ str_extract(., pattern = "\\-*\\d+\\.*\\d*"))) %>%
-      mutate(across(!ends_with(c("flag", "labdup", "sampleid")), # make extracted data numeric
+      mutate(across(!ends_with(c("flag", "qual", "labdup", "sampleid")), # make extracted data numeric
                     ~ as.numeric(.))) %>%
       #janitor::clean_names()  %>% # this is causing a problem by changing mu symbol to 'm'
       mutate(sample_depth = str_sub(sampleid, 6, 6)) %>% # get sample depth from sampleid
@@ -310,7 +322,7 @@ get_ada_data <- function(path, datasheet) {
       dplyr::rename(lake_id = sampleid) %>% # change name to match chemCoc
       mutate(site_id = "") %>% # create empty column for site_id (id is populated later)
       select(order(colnames(.))) %>% # alphabetize column names
-      select(lake_id, site_id, sample_filter, sample_depth, sample_type, labdup, everything()) # put id fields first
+      select(lake_id, site_id, sample_filter, sample_depth, sample_type, labdup, qual, everything()) # put id fields first
    
    return(maintable)
    
@@ -321,6 +333,11 @@ get_ada_data <- function(path, datasheet) {
 # 1. Read in chain of custody forms
 cin.ada.path <- paste0(userPath, 
                            "data/chemistry/nutrients/2020cinSentToAda/")
+
+lmp1 <- get_ada_data(cin.ada.path, "EPAGPA053,SS#7759,AE2.6,Forshay,LakeMethaneProject7-6-20,oP,GPKR.xls") %>%
+   conv_units(filename = "EPAGPA053,SS#7759,AE2.6,Forshay,LakeMethaneProject7-6-20,oP,GPKR.xls") %>%
+      mutate(site_id = "I don't know yet") %>% # add site_id
+      dup_agg # aggregate the lab duplicates (optional)
 
 
 cin.ada.coc <- rbind(read_excel(paste0(cin.ada.path,
