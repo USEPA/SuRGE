b2778ef16ac430bbf041f893ba3cae30ba0ea9af
Beaulieu <Beaulieu.Jake@epa.gov>
2022-03-03 16:19:58 -0500

    Updated lake report template.  In progress....

--- scripts/analysis/mergeChemistryFieldSheets.R
+++ scripts/analysis/mergeChemistryFieldSheets.R
@@ -11,12 +11,12 @@ class(chemistry$lake_id) == class(fld_sheet$lake_id) # TRUE
 class(chemistry$site_id) == class(fld_sheet$site_id) # TRUE
 
 # Check dimensions
-dim(chemistry) # 153, 73 [2/9/2022]
-dim(fld_sheet) # 1020, 73 [2/9/2022]
+dim(chemistry) # 152, 133 [3/1/2022]
+dim(fld_sheet) # 1036, 72 [3/1/2022]
 
 # Check for correspondence among unique identifiers
 # 8 lakes in chemistry, but not in field sheets.  These are all
-# R10 lakes from 2018.  As of 2/9/2022, these field sheets
+# R10 lakes from 2018.  As of 3/1/2022, these field sheets
 # haven't been read in yet.
 chemistry %>% filter(!(lake_id %in% fld_sheet$lake_id)) %>%
   select(lake_id) %>% distinct() %>% print(n=Inf)
@@ -27,4 +27,7 @@ fld_sheet %>% filter(!(lake_id %in% chemistry$lake_id)) %>%
   select(lake_id) %>% distinct() %>% print(n=Inf)
 
 chem_fld <- full_join(chemistry, fld_sheet)
-dim(chem_fld) # 1114, 207
+dim(chem_fld) # 1126, 203 [3/1/2022]
+
+# write to disk for reference in lake reports
+save(chem_fld, file = "output/chem_fld.RDATA")
