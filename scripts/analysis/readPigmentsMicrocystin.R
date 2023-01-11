ada6b6f5d85f5fb4b24366cc791f67dab5117758
Corra <Corra.Joseph@epa.gov>
2023-01-11 11:16:12 -0500

    Updated readPigmentsMicrocystin.R to read in 2022 data, including _flag fields.

--- scripts/analysis/readPigmentsMicrocystin.R
+++ scripts/analysis/readPigmentsMicrocystin.R
@@ -1,7 +1,8 @@
 # script for reading chlorophyll, phycocyanin, and microcystin measured
 # at Narragansett laboratory.
 
-
+# 11 Jan 2023: Flags already added to data by Jeff Hollister. 
+# Commenting out all code related to determining flags. (jwc) 
 
 
 # Jeff Hollister: the values are the concentration in the sample 
@@ -13,39 +14,35 @@ get_chla_data <- function(path, data, sheet) {
     mutate(site_id = as.numeric(gsub(".*?([0-9]+).*", "\\1", site))) %>% # convert to numeric
     mutate(sample_depth = "shallow") %>% # all samples were collected near a-w interface
     mutate(units = "ug_l") %>% # to match naming conventions
+
+    # mutate(chla_qual = "H") %>% # qual (holding time) flag
+    # mutate(chla_ship = "S") %>% # shipping flag 
     
-    # PLACEHOLDERS until data available 
-    mutate(chla_qual = "H") %>% # qual (holding time) flag
-    mutate(chla_ship = "S") %>% # shipping flag 
-    
-    rename(sample_type = reps, 
+    rename(sample_type = field_dups, 
            lake_id = waterbody, 
            chla_units = units, 
            chla = value)  %>%
+    
     # mutate(hold_time = collection_date - extraction_date) %>% # get hold time
     # mutate(chla_qual = case_when( # flag with "H" if hold time exceeded
     #   hold_time > 60 ~ "H",
     #   TRUE   ~ "")) %>%
-    # No detection limits reported for 2020-2021 chlorophyll data
-
+    
     # Create visit field
     mutate(visit = if_else(lake_id %in% c("281", "250") &
                              between(date %>% as.Date(format = "%m/%d/%Y"),
                                      as.Date("2022-08-15"),
                                      as.Date("2022-09-15")),
                            2, 1, missing = 1)) %>%
-
     select(lake_id, site_id, sample_type, sample_depth, visit,
-           chla, chla_units, chla_qual, chla_ship) %>%
-    unite("chla_flags", chla_qual, chla_ship, sep = " ") # merge all flag columns
-    # This data currently has placeholder flags (as of 9/26/2022)
-    # Until data are available, the following 4 lines are commented out:
-  # %>%
-  #   mutate(chla_flags = if_else( # NA if there are no flags
-  #     str_detect(chla_flags, "\\w"), chla_flags, NA_character_) %>%
-  #       str_squish()) # remove any extra white spaces
+           chla, chla_units, chla_flag) 
 
-  return(d)
+    # unite("chla_flags", chla_qual, chla_ship, sep = " ") %>% 
+    #   mutate(chla_flags = if_else( # NA if there are no flags
+    #     str_detect(chla_flags, "\\w"), chla_flags, NA_character_) %>%
+    #       str_squish()) # remove any extra white spaces
+    
+    return(d)
   
 }
 
@@ -57,20 +54,18 @@ get_phyco_data <- function(path, data, sheet) {
     mutate(sample_depth = "shallow") %>% # all samples were collected near a-w interface
     mutate(units = "ug_l") %>% # to match naming conventions
     
-    # PLACEHOLDERS until data available 
-    # See https://github.com/jhollist/surge_algal/issues
-    mutate(phycocyanin_qual = "H") %>% # qual (holding time) flag
-    mutate(phycocyanin_ship = "S") %>% # shipping flag 
+    # mutate(phycocyanin_qual = "H") %>% # qual (holding time) flag
+    # mutate(phycocyanin_ship = "S") %>% # shipping flag 
     
-    rename(sample_type = reps, 
+    rename(sample_type = field_dups, 
            lake_id = waterbody, 
            phycocyanin_units = units, 
            phycocyanin = value)  %>%
+    
     # mutate(hold_time = collection_date - extraction_date) %>% # get hold time
     # mutate(chla_qual = case_when( # flag with "H" if hold time exceeded
     #   hold_time > 60 ~ "H",
     #   TRUE   ~ "")) %>%
-    # No detection limits reported for 2020-2021 phycocyanin data
     
     # Create visit field
     mutate(visit = if_else(lake_id %in% c("281", "250") &
@@ -78,19 +73,16 @@ get_phyco_data <- function(path, data, sheet) {
                                      as.Date("2022-08-15"),
                                      as.Date("2022-09-15")),
                            2, 1, missing = 1)) %>%
-    
     select(lake_id, site_id, sample_type, sample_depth, visit, 
-           phycocyanin, phycocyanin_qual, phycocyanin_units, phycocyanin_ship) %>%
-    unite("phycocyanin_flags", phycocyanin_qual, 
-          phycocyanin_ship, sep = " ") # merge all flag columns
-  # This data currently has placeholder flags (as of 9/26/2022)
-  # Until data are available, the following 5 lines are commented out:
-  # %>%
-  #   mutate(phycocyanin_flags = if_else( # NA if there are no flags
-  #     str_detect(phycocyanin_flags, "\\w"), 
-  # phycocyanin_flags, NA_character_) %>%
-  #       str_squish()) # remove any extra white spaces
-
+           phycocyanin, phycocyanin_units, phyco_flag) 
+    
+    # unite("phycocyanin_flags", phycocyanin_qual, 
+    #       phycocyanin_ship, sep = " ") %>% # merge all flag columns:
+    #   mutate(phycocyanin_flags = if_else( # NA if there are no flags
+    #     str_detect(phycocyanin_flags, "\\w"), 
+    # phycocyanin_flags, NA_character_) %>%
+    #       str_squish()) # remove any extra white spaces
+    
   
   return(d)
   
@@ -103,37 +95,37 @@ cin.pig.path <- paste0(userPath,
 
 
 chla_20_21_22 <- get_chla_data(cin.pig.path,
-                               "surge_chla_all_2020_2021.csv") 
+                               "surge_chla_all_2020_2021_2022.csv") 
 
 phycocyanin_20_21_22 <- get_phyco_data(cin.pig.path,
-                            "surge_phyco_all_2020_2021.csv")
+                            "surge_phyco_all_2020_2021_2022.csv")
 
 
 pigments_20_21_22 <- left_join(chla_20_21_22, phycocyanin_20_21_22, 
                             by = c("lake_id", "site_id", 
-                                   "sample_depth", "sample_type", "visit")) %>%
-  select(-chla_flags, -phycocyanin_flags) # exclude these fields for now. 3/29/2022
-
+                                   "sample_depth", "sample_type", "visit")) 
 
 
 
 # SAMPLE INVENTORY------------------------
 # Read list of samples received by NAR.
 nar.samples <- read_excel(paste0(userPath,
-                                 "data/sampleTrackingSheets//NAR algal indicator//",
-                                 "narSampleReceiptList.xlsx"))
+         "data/sampleTrackingSheets//NAR algal indicator//",
+         "narSampleReceiptList.xlsx"))
 
 # Were all samples received expected?  Compare list of received samples to 
 # those expected. YES. [10/14/2022]
 setdiff(nar.samples[c("lake_id", "analyte", "sample_type")],
-        chem.samples.foo[c("lake_id", "analyte", "sample_type")]) %>% print(n=Inf)
+        chem.samples.foo[c("lake_id", "analyte", "sample_type")]) %>% 
+  print(n=Inf)
 
 
 # Have all NAR algae samples in comprehensive sample list been delivered to NAR?
 # Missing lake 204 samples.  Confirmed missing by NAR.  Sample found on lab floor
 # and framed in Jeff's office! 
-setdiff(chem.samples.foo %>% filter(analyte_group == "algae.nar", 
-                                    sample_year != 2018) %>% # 2018 R10 not sent to NAR
+setdiff(chem.samples.foo %>% 
+          filter(analyte_group == "algae.nar", 
+                 sample_year != 2018) %>% # 2018 R10 not sent to NAR
           select(lake_id, analyte, sample_type),
         nar.samples[c("lake_id", "analyte", "sample_type")]) %>%
   arrange(lake_id) %>% print(n=Inf)
