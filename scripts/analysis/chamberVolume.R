eca12b41fa2e8f42e1334a87f3da3a756cea151b
Beaulieu <Beaulieu.Jake@epa.gov>
2023-12-22 11:50:01 -0500

    minor updates to sample counts in aggregate file.  Changed chm_Vol to chm_vol in volume script.

--- scripts/analysis/chamberVolume.R
+++ scripts/analysis/chamberVolume.R
@@ -8,10 +8,10 @@
 
 # JC 3/13/2023: I think this produces the desired outcome:
 fld_sheet  <- fld_sheet %>% 
-  mutate(chmVol.L = (42.057 + (-0.2189 * rowMeans(
+  mutate(chm_vol_l = (42.057 + (-0.2189 * rowMeans(
   select(., chamb_grad_a, chamb_grad_b), na.rm = TRUE)))) %>%
   # NaN may cause errors? 
-  mutate(chmVol.L = if_else(is.na(chmVol.L), NA_real_, chmVol.L))
+  mutate(chm_vol_l = if_else(is.na(chm_vol_l), NA_real_, chm_vol_l))
 
 # ADA 
 #has a different design and will supply guidance to convert graduations to volume
