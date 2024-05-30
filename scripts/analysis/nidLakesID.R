46746b4bbc2146792f6e212e6b2497c8843bbf60
jbeaulie <beaulieu.jake@epa.gov>
2024-05-30 15:15:36 -0400

    Reduced NID data to unique year_completed field for each waterbody

--- scripts/analysis/nidLakesID.R
+++ scripts/analysis/nidLakesID.R
@@ -2,8 +2,13 @@
 # PROVIDED BY DEEMER 11/28/2023, MISSING LAKES SUBSEQUENTLY UPDATED BY PEGASUS
 nid_link <- read_csv(paste0(userPath, "data/siteDescriptors/nid_links_and_dam_age.csv")) %>%
   janitor::clean_names() %>%
-  rename(lake_id = site_id)
+  rename(lake_id = site_id) %>%
+  group_by(lake_id) %>%
+  slice_min(year_completed) %>% # dups for multiple dams on same lake constructed at same year
+  distinct(year_completed)
 
+dim(nid_link_min) #147
+  
 
 nid <- read_csv("https://nid.sec.usace.army.mil/api/nation/csv", skip = 1) %>%
   janitor::clean_names()
