e92d8dd10963351d788f0e0212b4cc6b93f3f685
rmpilla <pillarm@ornl.gov>
2024-07-15 16:52:34 -0400

    update hydroLakes link to replace grand_id from "0" to "NA", and add columns from hydroLakes database to manually linked waterbodies

--- scripts/analysis/hydroLakesID.R
+++ scripts/analysis/hydroLakesID.R
@@ -8,7 +8,7 @@
 # library(readxl)
 # library(hydrolinks) # add to renv output
 
- # read in SuRGE site information (lat/long required) and filter to sampled sites
+#  # read in SuRGE site information (lat/long required) and filter to sampled sites
 # surge_sites <- read_xlsx(paste0(userPath, "surgeDsn/SuRGE_design_20191206_eval_status.xlsx")) %>%
 #   filter(`EvalStatus Code` == "S") %>%
 #   janitor::clean_names() %>%
@@ -17,10 +17,10 @@
 #            as.numeric()) # convert lake_id to numeric
 # 
 # dim(surge_sites) # 147 sites
-# 
-# # use lat/long to find associated hylak_id from hydrolinks package
-# # using a buffer of 20 m from shapefile boundaries
-# # NOTE: this piece of code will take  a little while to run
+# # 
+# # # use lat/long to find associated hylak_id from hydrolinks package
+# # # using a buffer of 20 m from shapefile boundaries
+# # # NOTE: this piece of code will take  a little while to run
 # hydrolakes_surge <- link_to_waterbodies(surge_sites$lat_dd83,
 #                                        surge_sites$lon_dd83,
 #                                        surge_sites$lake_id,
@@ -29,23 +29,31 @@
 # dim(hydrolakes_surge) # matched 123 SuRGE sites
 # 
 # # hardcode in additional SuRGE sites (i.e., from map viewer)
-# surge_additions <- data.frame("lake_id" = c(281,
-#                                             69,
-#                                             207,
-#                                             98),
-#                               "hylak_id" = c(1030644,
-#                                              65,
-#                                              1055540,
-#                                              112928))
+# surge_manual <- data.frame("lake_id" = c(281,
+#                                          69,
+#                                          207,
+#                                          98),
+#                            "hylak_id" = c(1030644,
+#                                           65,
+#                                           1055540,
+#                                           112928))
+# 
+# surge_additions = get_shape_by_id(match_id = surge_manual$hylak_id, 
+#                                   feature_type = "waterbody", dataset = "hydrolakes") %>%
+#   as.data.frame() %>%
+#   select(-geometry) %>%
+#   mutate(MATCH_ID = surge_manual$lake_id)
+# 
 # 
 # # merge final dataset with hylak_id
-# hylak_link <- full_join(hydrolakes_surge, surge_additions,
-#                                by = c("MATCH_ID" = "lake_id", "hylak_id")) %>%
+# hylak_link <- full_join(hydrolakes_surge, surge_additions) %>%
 #   dplyr::select(-contains("pour"), -centroid_x, -lake_name, -country, - continent, -poly_src) %>%
 #   clean_names() %>%
 #   rename(lake_id = match_id) %>%
 #   relocate(lake_id, hylak_id, grand_id, lake_type) %>%
-#   rename_with(.cols = -c(lake_id, hylak_id, grand_id), ~paste0("hylak_", .))
+#   rename_with(.cols = -c(lake_id, hylak_id, grand_id), ~paste0("hylak_", .)) %>%
+#   mutate(grand_id = case_when(grand_id == 0 ~ NA,
+#                               TRUE ~ grand_id))
 # 
 # dim(hylak_link) #127 matches
 # 
