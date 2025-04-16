fdb52c789ad8fc9ef225ee495fc26d35ebdf2fef
jbeaulie <beaulieu.jake@epa.gov>
2025-04-16 10:24:32 -0400

    Finalized code for draft k600 figure for data paper

--- scripts/analysis/data_paper/dataPaperNumbers.R
+++ scripts/analysis/data_paper/dataPaperNumbers.R
@@ -79,15 +79,63 @@ dissolved_gas_k %>%
   summarize(n = n())
 
 # lit values
-read_xlsx(paste0("scripts/analysis/data_paper/",
-                          "27_2020_729_MOESM5_ESM.xlsx"), skip = 1) %>%
+k600_lit <- read_xlsx(paste0("scripts/analysis/data_paper/",
+                             "27_2020_729_MOESM5_ESM.xlsx"), skip = 1) %>%
   janitor::clean_names() %>%
   rename(method = method_g_gas_injection_c_chamber_e_eddy_covariance_m_mass_balance) %>%
-  filter(method == "C") %>%
-  mutate(k600_reported_or_calculated = as.numeric(k600_reported_or_calculated)) %>%
-  #select(lake_name, k600_reported_or_calculated) %>% print(n=Inf)
-  group_by(lake_name) %>%
-  summarize(k_mean = mean(k600_reported_or_calculated, na.rm = TRUE)) %>% print(n=Inf)
+  mutate(k600 = as.numeric(k600_reported_or_calculated) * (24/100)) %>% # cm/h -> m/d
+  filter(method == "C", k600 != 0) %>%
+  select(lake_name, k600, lat_dd, long_dd) %>% 
+  mutate(study = "Literature") %>%
+  st_as_sf(., coords = c("long_dd", "lat_dd")) %>%
+  st_set_crs(4326)
+
+# how many lakes id dataset
+k600_lit %>% distinct(lake_name) %>% summarize(n=n()) # 48
+# where are they located? Europe, Asia, N and S America
+tmap::tm_shape(World) +
+  tm_polygons() +
+  tm_shape(k600_lit) +
+  tm_symbols()
+
+# descriptive statistics
+summary(k600_lit$k600) # lit values
+summary(dissolved_gas_k %>% # SuRGE data
+          # rowwise needed to calculate mean across two columns, by row
+          rowwise() %>% #
+          mutate(k600 = mean(c(k_co2_600, k_ch4_600), na.rm = TRUE)) %>%
+          pull(k600))
+
+# k600 figure
+
+bind_rows(
+  # literature data
+  k600_lit,
+  
+  # SuRGE data
+  dissolved_gas_k %>%
+    select(lake_id, k_co2_600, k_ch4_600) %>%
+    rowwise() %>% # needed to calculate mean across two columns, by group
+    mutate(k600 = mean(c(k_co2_600, k_ch4_600), na.rm = TRUE)) %>%
+    ungroup() %>% # turn off rowwise grouping
+    select(-k_co2_600, -k_ch4_600) %>%
+    rename(lake_name = lake_id) %>%
+    mutate(study = "SuRGE")
+) %>%
+  filter(is.finite(k600)) %>% # omit NaN and avoid ggplot warning
+  ggplot(aes(k600, study)) +
+  geom_boxplot() +
+  scale_x_log10(breaks = c(0.01, 0.1, 1, 10), 
+                limits = c(0.01, 50), 
+                labels = c(0.01, 0.1, 1, 10)) +
+  xlab(expression(k[600]~(m~d^-1))) +
+  theme_bw() +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+ggsave(filename = "scripts/analysis/data_paper/k600.tiff", width = 3, height = 2, units = "in")
+  
 
 
 
